963c70926ac8c5f00c64c89e154719259cee4939
Gullit Miranda <gullitmiranda@requestdev.com.br>
2013-08-14 19:30:28 -0400

    updates

--- Sublime-HTMLPrettify/HTMLPrettify.py
+++ Sublime-HTMLPrettify/HTMLPrettify.py
@@ -11,20 +11,22 @@ class HtmlprettifyCommand(sublime_plugin.TextCommand):
 
   def prettify(self, edit):
     scriptPath = sublime.packages_path() + "/Sublime-HTMLPrettify/scripts/run.js"
-    setings = ' '.join([
-      "indent_size:\ 2",
-      "indent_char:\ ' '",
-      "max_char:\ 80",
-      "brace_style:\ collapse"
+    filePath = self.view.file_name()
+    setings = ','.join([
+      "indent_size: 1",
+      "indent_char: \t",
+      "max_char: 80",
+      "brace_style: collapse"
     ])
-    cmd = ["node",scriptPath,self.view.file_name(),setings]
 
-    if sublime.platform()=='windows':
-      p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
-      html = p.communicate()[0]
+    cmd = ["/usr/local/bin/node", scriptPath, filePath, setings]
+
+    if sublime.platform() == 'windows':
+      p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+      output = p.communicate()[0]
     else:
-      html = commands.getoutput('"'+'" "'.join(cmd)+'"')
+      output = commands.getoutput('"' + '" "'.join(cmd) + '"')
 
-    if len(html) > 0:
-      self.view.replace(edit, sublime.Region(0, self.view.size()), html.decode('utf-8'))
+    if len(output) > 0:
+      self.view.replace(edit, sublime.Region(0, self.view.size()), output.decode('utf-8'))
       sublime.set_timeout(self.save, 100)
